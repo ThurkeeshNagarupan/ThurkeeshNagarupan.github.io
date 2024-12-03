ebd82df5784b5dd01a01094c6dd376d0ee28bdd9
ThurkeeshNagarupan <thurkeesh.nagarupan@dcmail.ca>
2024-12-02 20:28:51 -0500

    Update main.js
    Deleted the current update method

--- Lab4/part3/main.js
+++ Lab4/part3/main.js
@@ -60,26 +60,7 @@ update() {
     this.x += this.velX;
     this.y += this.velY;
   }
-Ball.prototype.update = function() {
-  if((this.x + this.size) >= width) {
-    this.velX = -(this.velX);
-  }
 
-  if((this.x - this.size) <= 0) {
-    this.velX = -(this.velX);
-  }
-
-  if((this.y + this.size) >= height) {
-    this.velY = -(this.velY);
-  }
-
-  if((this.y - this.size) <= 0) {
-    this.velY = -(this.velY);
-  }
-
-  this.x += this.velX;
-  this.y += this.velY;
-};
 
 // define ball collision detection
 
