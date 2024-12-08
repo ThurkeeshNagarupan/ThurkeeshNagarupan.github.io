1e3c5b99168906496c0ed74ca79a9629c65ee628
ThurkeeshNagarupan <thurkeesh.nagarupan@dcmail.ca>
2024-12-07 23:45:48 -0500

    Update main.js
    Removed the second eventlistener since  showHideBtn.onclick = function() { already toggles the visibility of the comment section and updates the button text.

--- assessment-files/assessment-files/main.js
+++ assessment-files/assessment-files/main.js
@@ -45,14 +45,50 @@ function submitComment() {
   nameField.value = '';
   commentField.value = '';
 }
-document.querySelector('.show-hide').addEventListener('click', function () {
-    const commentWrapper = document.querySelector('.comment-wrapper');
-    if (commentWrapper.style.display === 'none' || !commentWrapper.style.display) {
-        commentWrapper.style.display = 'block';
-        this.textContent = 'Hide comments';
-    } else {
-        commentWrapper.style.display = 'none';
-        this.textContent = 'Show comments';
-    }
-});
+// functionality for showing/hiding the comments section
 
+const showHideBtn = document.querySelector('.show-hide');
+const commentWrapper = document.querySelector('.comment-wrapper');
+
+commentWrapper.style.display = 'none';
+
+showHideBtn.onclick = function() {
+  let showHideText = showHideBtn.textContent;
+  if(showHideText === 'Show comments') {
+    showHideBtn.textContent = 'Hide comments';
+    commentWrapper.style.display = 'block';
+  } else {
+    showHideBtn.textContent = 'Show comments';
+    commentWrapper.style.display = 'none';
+  }
+};
+
+// functionality for adding a new comment via the comments form
+
+const form = document.querySelector('.comment-form');
+const nameField = document.querySelector('#name');
+const commentField = document.querySelector('#comment');
+const list = document.querySelector('.comment-container');
+
+form.onsubmit = function(e) {
+  e.preventDefault();
+  submitComment();
+};
+
+function submitComment() {
+  const listItem = document.createElement('li');
+  const namePara = document.createElement('p');
+  const commentPara = document.createElement('p');
+  const nameValue = nameField.value;
+  const commentValue = commentField.value;
+
+  namePara.textContent = nameValue;
+  commentPara.textContent = commentValue;
+
+  list.appendChild(listItem);
+  listItem.appendChild(namePara);
+  listItem.appendChild(commentPara);
+
+  nameField.value = '';
+  commentField.value = '';
+}
